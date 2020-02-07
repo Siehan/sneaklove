937bd703dd43d93f3ee6470902c9bc2df6ac7817
franck-treboit <ftreboit@nexity.fr>
2020-02-07 12:18:43 +0100

    index modified

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const tagModel = require("../models/Tag");
-const protectRoute = require("../middlewares/protectRoute");
-const protectAdminRoute = require("../middlewares/protectAdminRoute");
+const sneakerModel = require("../models/Sneaker");
 
 router.get("/", (req, res) => {
-  res.render("index");
+   res.render("index");
 });
 
-//router.get("/sneakers/:cat", (req, res) => {
-//res.send("bar");
-//});
+router.get("/sneakers/collection", (req, res, next) => {
+    Promise.all([sneakerModel.find(), tagModel.find()])
+    .then(dbResults => {
+        res.render("products", {
+         sneakers : dbResults[0],
+         tags : dbResults[1]
+        });
+    })
+    .catch(next);
+});
 
-router.get("/prod-manage", protectRoute,  (req, res, next) => {
+router.get("/prod-manage", (req, res, next) => {
     Promise.all([sneakerModel.find(), tagModel.find()])
     .then(dbResults => {
         res.render("products_manage", {
@@ -23,11 +29,28 @@ router.get("/prod-manage", protectRoute,  (req, res, next) => {
     .catch(next);
 });
 
-router.get("/one-product/:id", (req, res) => {
-  res.send("baz");
+
+router.get("/sneakers/:cat", (req, res, next) => {
+    Promise.all([sneakerModel.find( { "category" : req.params.cat } ) , tagModel.find()])
+    .then(dbResults => {
+        res.render("products", {
+         sneakers : dbResults[0],
+         tags : dbResults[1]
+        });
+    })
+    .catch(next);
 });
 
-router.get("/product-edit/:id", protectRoute,  (req, res, next) => {
+router.get("/one-product/:id", (req, res, next) => {
+    sneakerModel
+    .findById(req.params.id) // retreive all the documents in the artists collection
+    .then(dbResults => {
+        res.render("one_product",  { sneaker : dbResults });
+    })
+    .catch(next);
+});
+
+router.get("/product-edit/:id", (req, res, next) => {
     Promise.all([sneakerModel.findById( { "_id" : req.params.id } ) , tagModel.find()])
     .then(dbResults => {
         res.render("product_edit", {
@@ -38,7 +61,7 @@ router.get("/product-edit/:id", protectRoute,  (req, res, next) => {
     .catch(next);
 });
 
-router.get("/delete-product/:id" , protectRoute,  (req, res, next) => {
+router.get("/delete-product/:id" , (req, res, next) => {
    sneakerModel
     .findByIdAndDelete(req.params.id)
     .then(dbRes => {
@@ -50,18 +73,18 @@ router.get("/delete-product/:id" , protectRoute,  (req, res, next) => {
 
 
 router.get("/create-product", (req, res, next) => {
-  tagModel
-    .find()
+    tagModel
+    .find() // retreive all the documents in the artists collection
     .then(dbResults => {
-      res.render("products_add", {
-        tags: dbResults
+        res.render("products_add", {
+         tags : dbResults
       });
     })
     .catch(next);
 });
 
 router.post("/create-product", (req, res, next) => {
-    const newSneaker = req.body;
+  const newSneaker = req.body;
   if (req.body.image === "") newSneaker.image = undefined;
   sneakerModel
     .create(newSneaker) // use the model and try doc insertion in database
@@ -71,37 +94,46 @@ router.post("/create-product", (req, res, next) => {
     })
     .catch(next);
 });
+//      req.flash("success", "sneaker successfully created");
 
 router.post("/tag-add", (req, res, next) => {
-  const {
-    label
-  } = req.body;
+  const { label } = req.body;
   tagModel
     .create({
       label,
     })
-    .then(dbRes => {
-      res.redirect("/");
+    .then(dbRes => {        
+      req.flash("success", "Le tag s'est bien créé");  
+      res.redirect("/tag-add");
     })
     .catch(next);
 });
 
 router.post("/tag-add-mix", (req, res, next) => {
-  const {
-    label
-  } = req.body;
+  const { label } = req.body;
   tagModel
     .create({
       label,
     })
     .then(dbRes => {
-      res.redirect("/create-product");
+        req.flash("success", "Le sneaker automatique s'est bien créé");     
+        res.redirect("/create-product");
     })
     .catch(next);
 });
 
 router.get("/create-tag", (req, res) => {
-  res.render("tag_add");
+    res.render("tag_add");
 });
+
+router.get("/signup", (req, res) => {
+  res.send("sneak");
+});
+
+router.get("/signin", (req, res) => {
+  res.send("love");
+});
+
+
 
 module.exports = router;
